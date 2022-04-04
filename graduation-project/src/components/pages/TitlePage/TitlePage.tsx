6190f64f67bbd59dde2aba4936cf2f1bebf42b94
1voodoo <pixel0101@mail.ru>
2022-04-04 17:09:32 +0300

    fail

--- graduation-project/src/components/pages/TitlePage/TitlePage.tsx
+++ graduation-project/src/components/pages/TitlePage/TitlePage.tsx
@@ -1,10 +1,8 @@
-import { Button, CircularProgress, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@mui/material";
+import { Button, CircularProgress, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
 import { Box } from "@mui/system";
 import { FC, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import getBitcoin, { IBitcon } from "../../../api/getBitcoin";
-import { INews } from "../../../api/getNews";
 import { RootState } from "../../../store";
 import { getAllNews } from "../../../store/News/ActionCreator";
 import SiteTicker from "../SiteTicker/SiteTicker";
@@ -14,15 +12,18 @@ import style from "./TitlePage.module.scss";
 const TitlePage: FC = () => {
     const dispatch = useDispatch();
     const { allNews } = useSelector((state: RootState) => state.GetAllNews);
-
+    const handleChange = (event: any) => {
+        setNews(event.target.value);
+      };
     const SpaceNews = allNews?.filter(item => item.newsSite === 'SpaceNews');
     const Arstechnica = allNews?.filter(item => item.newsSite === 'Arstechnica');
     const Spaceflight = allNews?.filter(item => item.newsSite === 'Spaceflight Now');
     const Teslarati = allNews?.filter(item => item.newsSite === 'Teslarati');
     const Nasa = allNews?.filter(item => item.newsSite === 'NASA');
     const NasaSpaceflight = allNews?.filter(item => item.newsSite === 'NASA Spaceflight');
-    // const [lol, setLol] = useState(SpaceNews)
-   
+    const [news, setNews] = useState()
+    console.log(news);
+    
     // console.log(SpaceNews);
     
     const navigate = useNavigate();
@@ -37,6 +38,22 @@ const TitlePage: FC = () => {
     return (<>
                 <p>Today {new Date().toISOString().slice(0, 10)}</p>
                 <div className={style.wrapper}><h1 className={style.one}>TitlePage</h1></div>
+                <Box sx={{ minWidth: 120 }}>
+                        <FormControl fullWidth>
+                          <InputLabel id="demo-simple-select-label">Choose News</InputLabel>
+                          <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={news}
+                            label="Agedsdasdas"
+                            onChange={handleChange}
+                          >
+                            <MenuItem value={SpaceNews}>SpaceNews</MenuItem>
+                            <MenuItem value={20}>Twenty</MenuItem>
+                            <MenuItem value={30}>Thirty</MenuItem>
+                          </Select>
+                        </FormControl>
+                      </Box>
                 <div className={style.line}></div> 
                 <div className={style.wrapper}> 
                   <SiteTicker/> 

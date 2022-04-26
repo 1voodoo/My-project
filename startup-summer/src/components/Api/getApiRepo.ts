e0bf8a7860ea2d3ea5e7952ba489b4e77931ed7f
IHAR <pixel0101@mail.ru>
2022-04-26 17:09:42 +0300

    user info

---
+++ startup-summer/src/components/Api/getApiRepo.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export interface IRepo {
+    description: string;
+    name: string;
+    html_url: string;
+};
+
+const getApiRepo = async (name: string): Promise<IRepo[] | null> => {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${name}/repos`);
+        
+        return response.data; 
+    } catch (e) {
+        alert(e);
+        throw(e);
+    } 
+        
+}
+export default getApiRepo;
---
+++ startup-summer/src/components/Api/getApiUser.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+export interface IUsers{
+    id: string;
+    avatar_url: string;
+    name: string;
+    login: string;
+    public_repos: number;
+    followers: number;
+    following: number;
+    html_url: string;
+}
+
+
+const getApiUser = async (name: string): Promise<IUsers> => {
+    try {
+        const response = await axios.get(`https://api.github.com/users/${name}`);
+        
+        return response.data; 
+    } catch (e) {
+        alert(e);
+        throw(e);
+    } 
+        
+}
+export default getApiUser;
+
+// const getApiRepo = async (name: string): Promise<IUsers> => {
+//     try {
+//         const response = await axios.get(`https://api.github.com/users/${name}/repos`);
+        
+//         return response.data; 
+//     } catch (e) {
+//         alert(e);
+//         throw(e);
+//     } 
+        
+// }
+// export default getApiRepo;

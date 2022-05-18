393045deb8abef63c92e1465fc6a0ae56450eb0e
IHAR <pixel0101@mail.ru>
2022-05-18 19:33:59 +0300

    loader

--- startup-summer/src/components/page/Error/Error.tsx
+++ startup-summer/src/components/page/Error/Error.tsx
@@ -1,10 +1,18 @@
 import { FC } from "react";
+import { useNavigate } from "react-router-dom";
 import style from './Error.module.scss';
+import Button from '@mui/material/Button';
 
 const Error: FC = () => {
+    const navigate = useNavigate();
+    const backHome = () => {
+        navigate('/')
+    };
+    
     return (
         <div className={style.error}>
-            Error
+           <h1 className={style.er}>Error 404</h1>
+           <Button onClick={backHome} >HOME</Button> 
         </div>
     );
 };

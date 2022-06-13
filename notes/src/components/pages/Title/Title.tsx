acb377b80097f08e86777d49f345fa63f4bfbc08
IHAR <pixel0101@mail.ru>
2022-06-13 19:38:00 +0300

    lesson 02

--- notes/src/components/pages/Title/Title.tsx
+++ notes/src/components/pages/Title/Title.tsx
@@ -1,21 +1,40 @@
 import { Button } from '@mui/material';
-import { prependListener } from 'process';
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useState } from 'react';
 import style from './Title.module.scss';
-export interface IRepo {
-  description: string;
-  tag: string;
-};
+
 
 const  Title:FC = () => {
+
+  // interface IRepo {
+  //   description: string;
+  //   tag: string;
+  // };
+  // interface IUser {
+  //   all: IRepo[];
+  // }
+  // const allNotes = [
+  //   {
+  //   }
+  // ]
+
   const [inputValue, setInputValue] = useState<string>('');
   const [dis, setDis] = useState(false);
-  const [arr, setArr] = useState<string[]>([]);
-  const [arr1, setArr1] = useState<IRepo[]>([]);
+  const [obj, setObj] = useState({});
+  const [arr, setArr] = useState([]);
+  const [arrSecond, setArrSecond] = useState<any[]>([]);
+  // const [arr5, setArr5] = useState<[]>([]);
   const [error, setError] = useState(false);
-  const [tag, setTag] = useState('');
-  console.log();
+  const [tagValue, setTagValue] = useState('');
+ console.log(obj);
+ 
   
+  
+
+  // arr!.push(obj);
+
+  // let copy = Object.assign(arr5);
+  // const add = {prop: inputValue, propTag: tagValue};
+
   const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(event.target.value);
     if(event.target.value.length < 20) {
@@ -24,36 +43,62 @@ const  Title:FC = () => {
       setError(true);
     }
   };
-  console.log(tag);
   
   const handleOnAdd = () => {
-    const tag = inputValue.indexOf('#');
-    const newTag = inputValue.slice(tag);
-    setTag(newTag);
-
-    if(inputValue.length != 0 && inputValue.length < 20) {
-      setArr([...arr, inputValue]);
-      setInputValue('');
+    if(inputValue.length != 0 && inputValue.length < 20 && !inputValue.includes("#")) {
+      setObj({name: inputValue, second: tagValue});
+      // arr.push(...arr, obj);
+      // setArr([obj, ...arr]);
+      // setArr(arr);
+      // setArr(obj);
+      // copy.push(add);
+      // setArr5(arr5);
       setError(false);
-    } else {
+      setInputValue('');
+    }
+
+    else if(inputValue.length != 0 && inputValue.length < 20 && inputValue.includes("#")) {
+      const tag = inputValue.indexOf('#');
+      const newTag = inputValue.slice(tag);
+      let result = inputValue.slice(0, tag);
+      setTagValue(newTag);
+      setInputValue(result);
+      setObj({name: result, second: newTag});
+      // arr.push(...arr, obj);
+      // setArr([obj, ...arr]);
+      // arr.push(...arr, obj);
+      // setArr(arr);
+      // setArr(obj);
+      // arr.push(obj);
+      // setArr(arr);
+      // setArr([...arr!, result]);
+      // copy.push(add);
+      // setArr5(arr5);
+      setError(false);
+      setTagValue('');
+      setInputValue('');
+      
+    }
+
+    else {
       setError(true);
     };
     
   };
 
-  const handleOnDelet = (item: string) => {
-      setArr(previous => previous.filter(current => current !== item));
-  };
+  // const handleOnDelet = (value: string) => {
+  //     setArr(previous => previous!.filter(current => current !== value));
+  // };
   
-  const handleOnChanges = (item: string, index: number) => {
-    setDis(false);
-    setInputValue(item);
-    setArr([...arr.slice(0, index), inputValue, ...arr.slice(index + 1)]);
-    if(inputValue.length === 0) {
-      setDis(true);
-    };
+  // const handleOnChanges = (value: string, index: number) => {
+  //   setDis(false);
+  //   setInputValue(value);
+  //   setArr([...arr!.slice(0, index), inputValue, ...arr!.slice(index + 1)]);
+  //   if(inputValue.length === 0) {
+  //     setDis(true);
+  //   };
     
-  };  
+  // };  
 
   // useEffect(() => {
   //   localStorage.setItem('arr', JSON.stringify(arr));
@@ -75,15 +120,15 @@ const  Title:FC = () => {
          
             {arr && (
               <div className={style.content}>
-                {arr.map((item, index)=> (
+                {/* {arr.map((value, index)=> (
                   <div className={style.container} key={index}>
-                    {item}
+                    {value}
                     <div className={style.btnContainer}>
-                      <button className={style.btnDelet} onClick={() => handleOnDelet(item)}>x</button>
-                      <button onClick={() => handleOnChanges(item, index)}>{dis ? 'add changes': 'change' }</button>
+                      <button className={style.btnDelet} onClick={() => handleOnDelet(value)}>x</button>
+                      <button onClick={() => handleOnChanges(value, index)}>{dis ? 'add changes': 'change' }</button>
                     </div>
                   </div>
-                ))}
+                ))} */}
               </div>
             )}
       </div>      
